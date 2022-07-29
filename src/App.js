6cd4f7083c24c8cd32f9cb4380d75b67aa9fecad
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-29 17:41:07 +0600

    html element create dynamically

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person nayok='Sakib Khan' naika ='Opu Biswas'></Person>
+      <Person nayok='Manna' naika ='Purnima'></Person>
+      <Person nayok='Elias Kanchon' naika ='Moushumi'></Person>
+      <Person nayok='Dipjol' naika ='Bobita'></Person>
     </div>
   );
+}
+
+function Person(props){
+  var personStyle = {
+    border : '2px solid #b23bc5',
+    margin : '10px',
+    padding : '10px'
+  }
+  
+  return(
+    <div style={personStyle}>
+      <h2 style={{padding : '0px', margin : '0px'}}> {props.nayok}</h2>
+      <h3 style={{padding : '0px', margin : '0px'}}><span style={{fontSize : '20px', opacity : 0.6}}> Hero of</span> {props.naika}</h3>
+  </div>
+  )
 }
 
 export default App;
